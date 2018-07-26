a2b1fe604875dd019e3a84adddbe456b1a655c00
sarahb <sarahb@il.ibm.com>
2018-07-26 16:47:49 +0300

    code review

--- bin/upload.sh
+++ bin/upload.sh
@@ -4,14 +4,15 @@ readonly ROOT_DIR=$(cd $(dirname $(dirname $0)) && pwd)
 readonly OUT_DIR=$ROOT_DIR/out
 
 VERSION=$1
-IAM_TOKEN=$2
-URL="https://s3-api.us-geo.objectstorage.softlayer.net/shelldist/dist/$VERSION"
+URL=$2
+IAM_TOKEN=$3
+URL="$2"
 
 for file in "$OUT_DIR"/*
 do
-  if [[ $file != "$OUT_DIR/checksum" && $file != "OUT_DIR/plugins.json" ]]; then
+  if [[ $file != "$OUT_DIR/checksums" && $file != "$OUT_DIR/plugins.json" ]]; then
     binary=${file##*/}
-    cmd=`curl -v -X PUT "$URL/$binary-$VERSION" \
+    cmd=`curl -v -X PUT "$URL/$VERSION/$binary-$VERSION" \
         -H "x-amz-acl: public-read" \
         -H "Authorization: Bearer $IAM_TOKEN" \
         -H "Content-Type: application/octet-stream" \
