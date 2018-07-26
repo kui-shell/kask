79342dc722b7a74878b5f0e49300580ea0d5ccd3
sarahb <sarahb@il.ibm.com>
2018-07-26 16:25:41 +0300

    upload

--- bin/upload.sh
+++ bin/upload.sh
@@ -5,17 +5,17 @@ readonly OUT_DIR=$ROOT_DIR/out
 
 VERSION=$1
 IAM_TOKEN=$2
-HOST="s3-api.us-geo.objectstorage.softlayer.net"
+URL="https://s3-api.us-geo.objectstorage.softlayer.net/shelldist/dist/$VERSION"
 
 for file in "$OUT_DIR"/*
 do
   if [[ $file != "$OUT_DIR/checksum" && $file != "OUT_DIR/plugins.json" ]]; then
     binary=${file##*/}
-    cmd=`curl -v -X PUT "https://$HOST/shelldist/dist/$VERSION/$binary-$VERSION" \
+    cmd=`curl -v -X PUT "$URL/$binary-$VERSION" \
         -H "x-amz-acl: public-read" \
-        -H "Authorization: $IAM_TOKEN" \
+        -H "Authorization: Bearer $IAM_TOKEN" \
         -H "Content-Type: application/octet-stream" \
         --data-binary "@$file"`
-     $cmd
+     echo $cmd
   fi
 done
