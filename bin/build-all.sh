7a8b209223526599c787bcde4f8e78bfbf54cc62
Ilan Prager <ilanp@il.ibm.com>
2018-07-26 12:39:08 +0300

    build scripts and gitignore - https://github.ibm.com/composer/cloudshell/issues/3

---
+++ bin/build-all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+readonly ROOT_DIR=$(cd $(dirname $(dirname $0)) && pwd)
+readonly OUT_DIR=$ROOT_DIR/out
+
+rm -rf $OUT_DIR
+
+build() {
+    os=$1
+    arch=$2
+    GOARCH=$arch GOOS=$os $ROOT_DIR/bin/build.sh
+
+    nf="cloud-shell-$os-$arch"
+    if [ $os == "windows" ]; then
+        nf="$nf.exe"
+    fi
+    mv $OUT_DIR/cloud-shell "$OUT_DIR/$nf"
+}
+
+build windows amd64
+build windows 386
+# disable CGO for Linux
+CGO_ENABLED=0 build linux amd64
+CGO_ENABLED=0 build linux 386
+CGO_ENABLED=0 build linux ppc64le
+build darwin amd64
+
+shasum $OUT_DIR/*
